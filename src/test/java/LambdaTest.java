3f48ad1822cf317f062e1fb7b7f3a279c35363a4
Dennis He <chinamonkeyking@gmail.com>
2016-09-03 20:19:07 +0800

    Correct a name typo and add more scenario

--- src/test/java/LambdaTest.java
+++ src/test/java/LambdaTest.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 /**
  * Created by XiaoNiuniu on 9/3/2016.
  */
-public class LamdaTest {
+public class LambdaTest {
 
     static class MyRunnable implements Runnable {
 
@@ -20,24 +20,37 @@ public class LamdaTest {
     }
 
     @Test
-    public void testLamdaInThread() {
-        Thread thread1 = new Thread(new Runnable() {
+    public void testLambdaInThread() {
+
+        // A class implement Runnable
+        Thread thread1 = new Thread(new MyRunnable("Thread 1 - I am a new thread"));
+
+        // Anonymous class
+        Thread thread2 = new Thread(new Runnable() {
             public void run() {
-                System.out.println("Thread 1 - I am a new thread");
+                System.out.println("Thread 2 - I am a new thread");
             }
         });
 
-        Thread thread2 = new Thread(() -> { System.out.println("Thread 2 - I am a new thread"); });
+        // Lambda expression
+        Thread thread3 = new Thread(() -> { System.out.println("Thread 3 - I am a new thread"); });
 
-        Thread thread3 = new Thread(new MyRunnable("Thread 3 - I am a new thread"));
+        // Not sure how this is called
+        Thread thread4 = new Thread() {
+            public void run() {
+                System.out.println("Thread 4 - I am a new thread");
+            }
+        };
 
         thread1.start();
         thread2.start();
         thread3.start();
+        thread4.start();
         try {
             thread1.join();
             thread2.join();
             thread3.join();
+            thread4.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
