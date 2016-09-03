9580185a6e579bd787b7a87cc678ea67d648c75c
Dennis He <chinamonkeyking@gmail.com>
2016-09-03 19:33:14 +0800

    Modify the test method

--- src/test/java/LamdaTest.java
+++ src/test/java/LamdaTest.java
@@ -4,8 +4,23 @@ import org.junit.Test;
  * Created by XiaoNiuniu on 9/3/2016.
  */
 public class LamdaTest {
+
+    static class MyRunnable implements Runnable {
+
+        private String message;
+
+        public MyRunnable(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(message);
+        }
+    }
+
     @Test
-    public void testLamda() {
+    public void testLamdaInThread() {
         Thread thread1 = new Thread(new Runnable() {
             public void run() {
                 System.out.println("Thread 1 - I am a new thread");
@@ -14,11 +29,15 @@ public class LamdaTest {
 
         Thread thread2 = new Thread(() -> { System.out.println("Thread 2 - I am a new thread"); });
 
+        Thread thread3 = new Thread(new MyRunnable("Thread 3 - I am a new thread"));
+
         thread1.start();
         thread2.start();
+        thread3.start();
         try {
             thread1.join();
             thread2.join();
+            thread3.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
